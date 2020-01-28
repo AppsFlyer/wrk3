46eaeda562aa25c18be5abab11640de8e9ed7799
eranharel <harel.eran@gmail.com>
2020-01-28 17:08:33 +0200

    Fixed omitted count correctness, removed "biased latency", and improved readability

--- wrk3.go
+++ wrk3.go
@@ -8,7 +8,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"sync/atomic"
 	"time"
 
 	"github.com/codahale/hdrhistogram"
@@ -25,20 +24,18 @@ func Benchmark(target RequestFunc) {
 	var concurrency int
 	var throughput int
 	var duration time.Duration
-	var includeCo bool
 
 	flag.IntVar(&concurrency, "concurrency", 10, "level of benchmark concurrency")
 	flag.IntVar(&throughput, "throughput", 10000, "target benchmark throughput")
 	flag.DurationVar(&duration, "duration", 20*time.Second, "benchmark time period")
-	flag.BoolVar(&includeCo, "co", true, "print coordinated omission latency distribution")
 	flag.Parse()
 
 	fmt.Printf("running benchmark for %v...\n", duration)
 	result := benchmark(concurrency, throughput, duration, target)
-	printBenchResult(throughput, duration, result, includeCo)
+	printBenchResult(throughput, duration, result)
 }
 
-func printBenchResult(throughput int, duration time.Duration, result BenchResult, includeCoHistogram bool) {
+func printBenchResult(throughput int, duration time.Duration, result BenchResult) {
 	fmt.Println("benchmark results:")
 	fmt.Println("total duration: ", result.totalTime, "(target duration:", duration, ")")
 	fmt.Println("total requests: ", result.counter)
@@ -46,17 +43,13 @@ func printBenchResult(throughput int, duration time.Duration, result BenchResult
 	fmt.Println("omitted requests: ", result.omitted)
 	fmt.Println("throughput: ", result.throughput, "(target throughput:", throughput, ")")
 	fmt.Println("latency distribution:")
-	printHistogram(result.histogram)
-	if includeCoHistogram {
-		fmt.Println("co(coordinated omission) latency distribution:")
-		printHistogram(result.coHistogram)
-	}
+	printHistogram(result.latency)
 }
 
 func printHistogram(hist *hdrhistogram.Histogram) {
 	brackets := hist.CumulativeDistribution()
 
-	fmt.Println("Quantile    | Count     | Value(usec) ")
+	fmt.Println("Quantile    | Count     | Value ")
 	fmt.Println("------------+-----------+-------------")
 
 	for _, q := range brackets {
@@ -78,108 +71,108 @@ func sendHttpRequest() error {
 }
 
 type localResult struct {
-	errors      int
-	counter     int
-	histogram   *hdrhistogram.Histogram
-	coHistogram *hdrhistogram.Histogram
+	errors  int
+	counter int
+	latency *hdrhistogram.Histogram
 }
 
 type BenchResult struct {
-	throughput  float64
-	counter     int
-	errors      int
-	omitted     int
-	histogram   *hdrhistogram.Histogram
-	coHistogram *hdrhistogram.Histogram
-	totalTime   time.Duration
+	throughput float64
+	counter    int
+	errors     int
+	omitted    int
+	latency    *hdrhistogram.Histogram
+	totalTime  time.Duration
 }
 
 func benchmark(concurrency int, throughput int, duration time.Duration, sendRequest RequestFunc) BenchResult {
-	rateLimiter := rate.NewLimiter(rate.Limit(throughput), concurrency)
-
-	var omitted int64 = 0
 	eventsBuf := make(chan time.Time, 10000)
+	omittedChan := make(chan int, 1)
 	doneCtx, cancel := context.WithTimeout(context.Background(), duration)
-	go func() {
-		var _omitted int64 = 0
-		for err := rateLimiter.Wait(doneCtx); err == nil; err = rateLimiter.Wait(doneCtx) {
-			select {
-			case eventsBuf <- time.Now():
-			default:
-				_omitted += 1
-			}
-		}
 
-		atomic.StoreInt64(&omitted, _omitted)
-		close(eventsBuf)
-	}()
+	go generateEvents(throughput, concurrency, doneCtx, eventsBuf, omittedChan)
 
 	results := make(chan localResult, concurrency)
 	start := time.Now()
 	for i := 0; i < concurrency; i++ {
-		go func() {
-			res := localResult{
-				errors:      0,
-				counter:     0,
-				histogram:   createHistogram(),
-				coHistogram: createHistogram(),
-			}
-
-			done := false
-			for !done {
-				select {
-				case <-doneCtx.Done():
-					done = true
-				case t := <-eventsBuf:
-					res.counter += 1
-					start := time.Now()
-					err := sendRequest()
-					if err != nil {
-						res.errors += 1
-					}
-
-					if err = res.coHistogram.RecordValue(int64(time.Since(t))); err != nil {
-						log.Println("error reporting to hdr", err)
-					}
-
-					if err = res.histogram.RecordValue(int64(time.Since(start))); err != nil {
-						log.Println("error reporting to hdr", err)
-					}
-				}
-			}
-
-			results <- res
-		}()
+		go sendRequests(doneCtx, sendRequest, eventsBuf, results)
 	}
 
 	<-doneCtx.Done()
 	cancel()
 
+	return summarizeResults(concurrency, results, start, omittedChan)
+}
+
+func generateEvents(throughput int, concurrency int, doneCtx context.Context, eventsBuf chan time.Time, omittedChan chan int) {
+	omitted := 0
+	rateLimiter := rate.NewLimiter(rate.Limit(throughput), 2*concurrency)
+	for err := rateLimiter.Wait(doneCtx); err == nil; err = rateLimiter.Wait(doneCtx) {
+		select {
+		case eventsBuf <- time.Now():
+		default:
+			omitted++
+		}
+	}
+
+	close(eventsBuf)
+	omittedChan <- omitted
+}
+
+func sendRequests(doneCtx context.Context, sendRequest RequestFunc, eventsBuf <-chan time.Time, results chan localResult) {
+	res := localResult{
+		errors:  0,
+		counter: 0,
+		latency: createHistogram(),
+	}
+
+	done := false
+	for !done {
+		select {
+		case <-doneCtx.Done():
+			done = true
+		case t, ok := <-eventsBuf:
+			if ok {
+				res.counter++
+				err := sendRequest()
+				if err != nil {
+					res.errors++
+				}
+
+				if err = res.latency.RecordValue(int64(time.Since(t))); err != nil {
+					log.Println("failed to record latency", err)
+				}
+			} else {
+				done = true
+			}
+		}
+	}
+
+	results <- res
+}
+
+func summarizeResults(concurrency int, results <-chan localResult, start time.Time, omittedChan chan int) BenchResult {
 	counter := 0
 	errors := 0
-	histogram := createHistogram()
-	coHistogram := createHistogram()
+	latency := createHistogram()
 
 	for i := 0; i < concurrency; i++ {
 		localRes := <-results
 		counter += localRes.counter
 		errors += localRes.errors
-		histogram.Merge(localRes.histogram)
-		coHistogram.Merge(localRes.coHistogram)
+		latency.Merge(localRes.latency)
 	}
 
 	totalTime := time.Since(start)
 
 	return BenchResult{
-		throughput:  float64(counter) / totalTime.Seconds(),
-		counter:     counter,
-		errors:      errors,
-		omitted:     int(omitted),
-		histogram:   histogram,
-		coHistogram: coHistogram,
-		totalTime:   totalTime,
+		throughput: float64(counter) / totalTime.Seconds(),
+		counter:    counter,
+		errors:     errors,
+		omitted:    <-omittedChan,
+		latency:    latency,
+		totalTime:  totalTime,
 	}
-
 }
 
 func createHistogram() *hdrhistogram.Histogram {
